2bb0db358cb942393f2528bfd77e596ff44200f7
tothpeter1216 <tothpeter1216@gmail.com>
2021-01-07 13:00:40 +0100

    axios installed, data fetched in useEfect, useState works with codes, skillFileds, schools, hobbies

--- src/App.js
+++ src/App.js
@@ -6,8 +6,32 @@ import Hobbies from "./components/Hobbies";
 import EducationSection from "./components/EducationSection";
 import Slider from "./components/Slider";
 import { data } from "./data/dataHun";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [skillFields, setSkillFields] = useState([]);
+  const [schools, setSchools] = useState([]);
+  const [codes, setCodes] = useState([]);
+  const [hobbies, setHobbies] = useState([]);
+
+  useEffect(() => {
+    console.log("effect");
+
+    async function fetchData() {
+      // You can await here
+      const response = await axios.get("http://localhost:3001/api/all");
+      console.log(response.data.code);
+      setCodes(response.data.code);
+      setSchools(response.data.schools);
+      setSkillFields(response.data.skillField);
+      setHobbies(response.data.hobbie);
+
+      // ...
+    }
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
       <Header />
