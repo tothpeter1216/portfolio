649a9f0d908bff8e25e4611d25667f5778a52db9
tothpeter1216 <tothpeter1216@gmail.com>
2021-01-13 16:43:34 +0100

    Slider component changed and reusable with array

--- src/components/EducationSection.js
+++ src/components/EducationSection.js
@@ -1,13 +1,19 @@
 import React from "react";
 import Slider from "./Slider";
 import "./EducationSection.css";
+import Education from "./Education";
 
 const EducationSection = ({ schools }) => {
+  let sliderArr = [];
+  sliderArr = schools.map((school, index) => {
+    return <Education key={school.id} school={school} />;
+  });
+
   return (
     <div id="education-section" className="section">
       <h1>Képzések, iskolák</h1>
 
-      <Slider schools={schools} />
+      <Slider schools={schools} pagesArray={sliderArr} />
     </div>
   );
 };
--- src/components/Slider.js
+++ src/components/Slider.js
@@ -2,24 +2,20 @@ import React, { useState } from "react";
 import "./Slider.css";
 import Education from "./Education";
 
-const Slider = ({ schools }) => {
-  let sliderArr = [1, 2, 3, 4, 5];
-  sliderArr = schools.map((school, index) => {
-    return <Education key={school.id} school={school} />;
-  });
+const Slider = ({ pagesArray }) => {
   const [x, setX] = useState(0);
 
   const goLeft = () => {
-    x === 0 ? setX(-100 * (sliderArr.length - 1)) : setX(x + 100);
+    x === 0 ? setX(-100 * (pagesArray.length - 1)) : setX(x + 100);
   };
   const goRight = () => {
     // setX(x - 100);
-    x === -100 * (sliderArr.length - 1) ? setX(0) : setX(x - 100);
+    x === -100 * (pagesArray.length - 1) ? setX(0) : setX(x - 100);
   };
 
   return (
     <div className="slider">
-      {sliderArr.map((item, index) => {
+      {pagesArray.map((item, index) => {
         return (
           <div
             key={index}
