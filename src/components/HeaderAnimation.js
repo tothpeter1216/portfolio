8222e13150057c0aac8b7b1da6ff273828657455
tothpeter1216 <tothpeter1216@gmail.com>
2021-01-13 10:13:09 +0100

    better bg colors

--- src/components/HeaderAnimation.js
+++ src/components/HeaderAnimation.js
@@ -4,6 +4,12 @@ import "./HeaderAnimation.css";
 const HeaderAnimation = () => {
   return (
     <div className="headerAnimation">
+      <div className="light-squares">
+        <div className="light-square ls1"></div>
+        <div className="light-square ls2"></div>
+        <div className="light-square ls3"></div>
+      </div>
+
       <img src={"./bg1.png"} alt="house" className="bg-house bg1" />
       <img src={"./bg2.png"} alt="house" className="bg-house bg2" />
       <img src={"./bg3.png"} alt="house" className="bg-house bg3" />
