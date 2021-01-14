0d08b40354eaf09a11a04a1b3504fec5493c6e39
tothpeter1216 <tothpeter1216@gmail.com>
2021-01-14 12:59:06 +0100

    footer is ready

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div id="footer">
+      <p>Tóth Péter 2021</p>
+      <p>tothpeter1216@gmail.com</p>
+    </div>
+  );
+};
+
+export default Footer;
