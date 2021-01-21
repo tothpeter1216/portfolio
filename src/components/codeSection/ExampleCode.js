ab9ccf8593fbbdbab1581a1dc08e074e95449c21
tothpeter1216 <tothpeter1216@gmail.com>
2021-01-21 14:32:18 +0100

    github2 link added to codesection

--- src/components/codeSection/ExampleCode.js
+++ src/components/codeSection/ExampleCode.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 const ExampleCode = ({ code }) => {
-  const { name, description, technologies, gitHub, website } = code;
+  const {
+    name,
+    description,
+    technologies,
+    gitHub,
+    gitHub2,
+    website,
+    websiteLinkText,
+  } = code;
   return (
     <div className="code-content">
       <h2 className="code-name">{name}</h2>
@@ -11,8 +19,13 @@ const ExampleCode = ({ code }) => {
           return " * " + tech + " * ";
         })}
       </p>
-      <p>{website && <a href={website}>website</a>}</p>
+      <p>
+        {website && (
+          <a href={website}>{websiteLinkText ? websiteLinkText : "link"}</a>
+        )}
+      </p>
       <p>{gitHub && <a href={gitHub}>Codes on gitHub</a>}</p>
+      <p>{gitHub2 && <a href={gitHub2}>Codes on gitHub 2</a>}</p>
     </div>
   );
 };
